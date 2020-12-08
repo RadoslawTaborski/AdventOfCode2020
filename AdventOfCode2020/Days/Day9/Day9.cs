66fa40986268bb18c623a786962cc02b365549db
Radosław Tabrski <radoslaw.taborski@gmail.com>
2020-12-08 17:04:25 +0100

    Day9 prepared files

---
+++ AdventOfCode2020/Days/Day9/Day9.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode2020.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Days.Day9
+{
+    public abstract class Day9 : Task
+    {
+        protected override void Result(out string result)
+        {
+            base.Result(out result);
+
+            var input = ReadRows("Input1.txt");
+        }
+    }
+}
---
+++ AdventOfCode2020/Days/Day9/Task2.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2020.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days.Day9
+{
+    [Task(9,2)]
+    public class Task2 : Day9
+    {
+        
+    }
+}
