8486c62e2aa2cd8f00b774eadafac4aa42720acb
Radosław Tabrski <radoslaw.taborski@gmail.com>
2020-12-18 23:21:31 +0100

    Day19 prepared files

---
+++ AdventOfCode2020/Days/Day19/Task1.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2020.Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days.Day19
+{
+    [Puzzle(19)]
+    public class Task1 : Day19
+    {
+        
+    }
+}
