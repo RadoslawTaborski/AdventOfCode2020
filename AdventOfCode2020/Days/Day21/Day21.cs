88c2dae63f39b746db62852688048b981897b26a
Radosław Tabrski <radoslaw.taborski@gmail.com>
2020-12-22 18:45:52 +0100

    Day21 prepared files

---
+++ AdventOfCode2020/Days/Day21/Day21.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode2020.Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days.Day21
+{
+    public abstract class Day21 : Puzzle
+    {
+        protected override void Result(out string result)
+        {
+            base.Result(out result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Days/Day21/Task2.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2020.Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days.Day21
+{
+    [Puzzle(21,2)]
+    public class Task2 : Puzzle
+    {
+        
+    }
+}
