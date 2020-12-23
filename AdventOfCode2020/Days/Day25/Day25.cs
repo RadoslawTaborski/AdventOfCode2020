234bce1a87cfcd6c38669ed8993f5834efaec701
Radosław Tabrski <radoslaw.taborski@gmail.com>
2020-12-23 22:55:40 +0100

    Day24 and 25 prepared files

---
+++ AdventOfCode2020/Days/Day25/Day25.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode2020.Puzzles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days.Day25
+{
+    public abstract class Day25 : Puzzle
+    {
+        protected override void Result(out string result)
+        {
+            base.Result(out result);
+
+            var input = ReadRows("Input.txt");
+        }
+    }
+}
