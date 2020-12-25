039ea00c34adfaff41c27ea9df3c0454190ec6ea
Radosław Tabrski <radoslaw.taborski@gmail.com>
2020-12-25 23:37:54 +0100

    Day25 and whole event completed

--- AdventOfCode2020/Days/Day25/Day25.cs
+++ AdventOfCode2020/Days/Day25/Day25.cs
@@ -7,13 +7,43 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2020.Days.Day25
 {
-    public abstract class Day25 : Puzzle
+    [Puzzle(25)]
+    public class Day25 : Puzzle
     {
         protected override void Result(out string result)
         {
-            base.Result(out result);
+            var input = ReadRows("Input1.txt").Select(x => long.Parse(x));
 
-            var input = ReadRows("Input.txt");
+            var cardLoopSize = GenerateLoopSize(input.ElementAt(0), 1, 7, 20201227);
+            var doorLoopSize = GenerateLoopSize(input.ElementAt(1), 1, 7, 20201227);
+
+            var cardPrivateKey = GeneratePrivateKey(1, input.ElementAt(0), 20201227, doorLoopSize);
+
+            result = $"{cardPrivateKey}";
+        }
+
+        private int GenerateLoopSize(long searchValue, long initValue, long subjectNumber, long magicNumber)
+        {
+            var value = initValue;
+            var counter = 0;
+            while (value != searchValue)
+            {
+                value = (value * subjectNumber) % magicNumber;
+                counter++;
+            }
+            return counter;
+        }
+
+        private long GeneratePrivateKey(long initValue, long subjectNumber, long magicNumber, long loopSize)
+        {
+            var value = initValue;
+            var counter = 0;
+            while (counter != loopSize)
+            {
+                value = (value * subjectNumber) % magicNumber;
+                counter++;
+            }
+            return value;
         }
     }
 }
