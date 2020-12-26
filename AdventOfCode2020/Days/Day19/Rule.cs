dfc1190722e3fe4712e3a5b2f1ad87a0d30b055f
Radosław Tabrski <radoslaw.taborski@gmail.com>
2020-12-26 20:49:53 +0100

    Day19 fixed

--- AdventOfCode2020/Days/Day19/Rule.cs
+++ AdventOfCode2020/Days/Day19/Rule.cs
@@ -9,78 +9,29 @@ namespace AdventOfCode2020.Days.Day19
 {
     public class Rule : IRule
     {
-        private int counter = 0;
-        private Dictionary<string[], List<string>> history = new Dictionary<string[], List<string>>(new StringArrayEqualityComparer());
         public int Id { get; init; }
         public List<List<int>> Patterns { get; init; }
 
         public string GenerateRegex(Dictionary<int, IRule> dictionary)
         {
             var list = new List<string>();
-            var shouldBeLoop = Patterns.Any(x => x.Contains(Id));
-            if (shouldBeLoop)
+
+            foreach (var pattern in Patterns)
             {
-                list.Add(CreateFromCollectionWithLoop(Patterns[0], dictionary));
-            }
-            else
-            {
-                foreach (var pattern in Patterns)
-                {
-                    list.Add(CreateFromCollection(pattern, dictionary));
-                }
+                list.Add(CreateFromCollection(pattern, dictionary));
             }
 
             return new StringBuilder().Append('(').Append(string.Join("|", list)).Append(')').ToString();
-        }
-
-        private string CreateFromCollectionWithLoop(List<int> pattern, Dictionary<int, IRule> dictionary)
-        {
-            var strBuilder = new StringBuilder();
-
-            if (pattern.Count == 1)
-            {
-                strBuilder.Append("(").Append(dictionary[pattern[0]].GenerateRegex(dictionary)).Append(")+");
-            } else
-            {
-                var r1 = dictionary[pattern[0]].GenerateRegex(dictionary);
-                var r2 = dictionary[pattern[1]].GenerateRegex(dictionary);
-                strBuilder.Append("(").Append(Loop(r1,r2,0,100)).Append(")");
-            }
-
-            return strBuilder.ToString();
-        }
-
-        private string Loop(string r1, string r2, int v1, int v2)
-        {
-            if (v1 == v2)
-            {
-                return "";
-            }
-
-            var strBuilder = new StringBuilder();
-            var innerLoop = Loop(r1, r2, v1 + 1, v2);
-            if (innerLoop != "")
-            {
-                strBuilder.Append('(').Append(r1).Append(")((").Append(innerLoop).Append(")*)(").Append(r2).Append(")");
-            }
-            else
-            {
-                strBuilder.Append('(').Append(r1).Append(")(").Append(r2).Append(")");
-            }
-
-            return strBuilder.ToString();
         }
 
         private string CreateFromCollection(List<int> pattern, Dictionary<int, IRule> dictionary)
         {
             var strBuilder = new StringBuilder();
 
-            strBuilder.Append('(');
             foreach (var a in pattern)
             {
-                strBuilder.Append('(').Append(dictionary[a].GenerateRegex(dictionary)).Append(')');
+                strBuilder.Append(dictionary[a].GenerateRegex(dictionary));
             }
-            strBuilder.Append(')');
 
             return strBuilder.ToString();
         }
